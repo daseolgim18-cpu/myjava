30a60123e8a1bb4b52e134ae6d82a3be061ab3fe
daseol <daseolgim18@gmail.com>
2026-04-15 10:33:01 +0900

    example0415

---
+++ daseol/src/example0415/Ex04.java
@@ -0,0 +1,10 @@
+package example0415;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Car1 myCar = new Car1("검정", 3000);
+        //Car1 myCar = new Car(); (x)
+
+        System.out.println(myCar);
+    }
+}
